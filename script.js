7eadca09e116c69f180ea4e918c2b3ed2e976444
freitas8632 <freitas.ramon@escola.pr.gov.br>
2025-11-18 10:40:25 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const defaultConfig = {
    page_title: "Bem-vindo à Minha Página!",
+   
    subtitle: "Extrovertido, amigável e apaixonado por futebol",
    about_title: "Sobre Mim",
    routine_title: "Minha Rotina Diária",
@@ -145,5 +146,5 @@ const defaultConfig = {
    });
  }
 
- // Segundo Script (ofuscado, provavelmente para analytics ou anti-bot)
+ // Segundo Script (provavelmente para analytics ou anti-bot)
  (function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'9a078c5343ede018',t:'MTc2MzQ2OTc1MC4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();
